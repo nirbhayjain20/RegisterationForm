e10d87bca0bf8095e81b83ea869e1d7b9930f57b
Nirbhay Jain <nirbhayjain2000@yahoo.in>
2020-09-03 13:42:11 +0530

    form

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+
+const bodyParser = require('body-parser');
+
+app.set('view engine', 'ejs')
+app.use(bodyParser.urlencoded({ extended: false }))
+app.get('/', (req, res) => {
+  res.render('register')
+})
+
+app.post('/register',(req,res)=>{
+    var name = req.body.fname;
+    console.log(name);
+    res.send('success');
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
